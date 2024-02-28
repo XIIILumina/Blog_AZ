91653cf3b725085e2a6f6850e4d2e7f84cb1bb76
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-28 13:29:38 +0200

    first commit

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE "Blog_AZ";
+USE Blog_AZ;
+CREATE TABLE posts (id, title);
+INSERT INTO posts (title)
+VALUES ("My First Blog Post"),
+("My Second Blog Post");
+
---
+++ index.php
@@ -0,0 +1,2 @@
+<?
+echo "Tolik Shebenj";
